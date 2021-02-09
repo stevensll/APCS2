9bff0872051c5d77f27321b65566f961ec95be4c
Steven <deltaorb637@gmail.com>
2021-02-08 19:41:06 -0500

    Created countNoDoubleLetterWords, successfully tested it.

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -4,6 +4,26 @@ public class Recursion{
         else return reverse(s.substring(1)) + s.charAt(0);
     }
 
-
+    /*
+    *@param length how long the words must be
+    *param word the variable to store the partial solution (should start at "")
+    *@return the number of words that have no adjacent matching letters using the letters a-z.
+    *Repetition allowed except when letters are adjacent.
+    */
+    public static long countNoDoubleLetterWords(int length,String word){
+        long count = 0;
+        if (length  == 0) {
+            //System.out.println(word);
+            return 1;
+        }
+        else{
+            for(char i = 'a'; i <= 'z'; i++){
+                if(word.length() == 0 || word.charAt(word.length()-1) != i){
+                    count+=countNoDoubleLetterWords(length-1, word+i);
+                }
+            }
+        }
+        return count;
+    }
 
 }
--- 01-recursion/StevenRecursionTester.java
+++ 01-recursion/StevenRecursionTester.java
@@ -1,10 +1,20 @@
 public class StevenRecursionTester{
     public static void main(String[] args) {
+        /*reverse() string test
         System.out.println(Recursion.reverse("hello"));
         System.out.println(Recursion.reverse("michael"));
         System.out.println(Recursion.reverse("12345"));
         System.out.println(Recursion.reverse("9#(01"));
+        */
+        // countNoDoubleLetterWords() test.
+        for(int i = 0; i < 7; i++){
+            if(Recursion.countNoDoubleLetterWords(i+1,"")!=26* Math.pow(25,i)) System.out.println("Error at length " + i);
+            else System.out.println("Good at length " + i);
+        }
+        //
+
 
     }
+
 
 }
