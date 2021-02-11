95538a517abb147237311840818a17a1bbf16028
Steven <deltaorb637@gmail.com>
2021-02-11 14:15:05 -0500

    Created QueensBoard class, tester, and added method headers.

---
+++ 02-queens/QueensBoard.java
@@ -0,0 +1,42 @@
+public class QueensBoard {    
+
+        private int[][] board;
+
+        private boolean AddQueen(int r, int c){}
+
+        private void removeQueen(int r, int c){}
+            /**
+          *@return The output string formatted as follows:
+          *All numbers that represent queens are replaced with 'Q'
+          *all others are displayed as underscores '_'
+          *There are spaces between each symbol:
+          *_ _ Q _
+          *Q _ _ _
+          *_ _ _ Q
+          *_ Q _ _
+          *(pythonic string notation for clarity,
+          *excludes the character up to the *)
+          */
+          public String toString(){
+  
+            
+
+          }
+
+
+          /**Find the first solution configuration possible for this size board. Start by placing
+          *  the 1st queen in the top left corner, and each new queen in the next COLUMN. When backtracking
+          *  move the previous queen down to the next valid space. This means everyone will generate the same
+          *  first solution.
+          *@return false when the board is not solveable and leaves the board filled with zeros;
+          *        returns true when the board is solveable, and leaves the board in a solved state
+          *@throws IllegalStateException when the board starts with any non-zero value (e.g. you solved a 2nd time.)
+          */
+          public boolean solve(){}
+
+          /**Find all possible solutions to this size board.
+          *@return the number of solutions found, and leaves the board filled with only 0's
+          *@throws IllegalStateException when the board starts with any non-zero value (e.g. you ran solve() before this method)
+          */
+          public int countSolutions(){} 
+}
