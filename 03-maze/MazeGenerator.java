3de9b59e23eef5b1a37f1da86128b57f0da92657
Steven Lei <46882137+stevensll@users.noreply.github.com>
2021-03-02 23:49:39 -0500

    created generator file and initiaed nextSquare() method.

---
+++ 03-maze/MazeGenerator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class MazeGenerator {
+    public static void generate(char[][]maze, int rows, int cols,int startrow, int startcol){
+
+    }
+    /*nextSquare checks if the input square can be carved. it must satisfy
+      the condition that the there are less than 2 Neighbors (up/down/left/right) that are already carved.
+    */
+    private static boolean nextSquare(){
+        
+    }
+}
