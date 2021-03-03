32ea96dc686c6e6fce44790a1c0cd43c56b006b8
Steven Lei <46882137+stevensll@users.noreply.github.com>
2021-03-02 22:10:09 -0500

    Finished and successfully tested solve().

--- 03-maze/StevenTester.java
+++ 03-maze/StevenTester.java
@@ -11,16 +11,17 @@ public class StevenTester {
         // test.moveBack(7, 11);
         // System.out.println(test.solve());
         // System.out.println(test);
-        Maze data1 = new Maze("data1.dat");
-        Maze data2 = new Maze("data2.dat");
-        Maze data3 = new Maze("data3.dat");
-        Maze data5 = new Maze("data5.dat");
-        data5.setAnimate(true);
-        System.out.println(data1.solve());
-        System.out.println(data2.solve());
-        System.out.println(data3.solve());
-        System.out.println(data5.solve());
-
+        // Maze data1 = new Maze("data1.dat");
+        // Maze data2 = new Maze("data2.dat");
+        // Maze data3 = new Maze("data3.dat");
+        // Maze data5 = new Maze("data5.dat");
+        // data5.setAnimate(true);
+        // System.out.println(data1.solve());
+        // System.out.println(data2.solve());
+        // System.out.println(data3.solve());
+        // System.out.println(data5.solve());
+        Maze dataVari = new Maze("dataVari.dat");
+        System.out.println(dataVari.solve());
     }
     
 }
