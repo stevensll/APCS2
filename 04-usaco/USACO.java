59704c87d31c1ee1800c9b99e53054ba0543890b
Steven Lei <46882137+stevensll@users.noreply.github.com>
2021-03-06 00:41:53 -0500

    Created usaco file and started variable declaration for bronze.

---
+++ 04-usaco/USACO.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class USACO{
+    public static int bronze(String filename) throws FileNotFoundException{
+        Scanner in = new Scanner(new File (filename));
+        in.nextLine()
+        final int r;
+        final int c;
+        int target_elevation;
+        final int area = 5184;
+        int sol = 0;    
+        return sol;
+    }
+    public static long silver(String filename) throws FileNotFoundException{
+    
+    }
+}
+
+//Example Usage:
+// int ans = USACO.bronze("lake1.input");
+// long ans2 = USACO.silver("cowPath1.input");
