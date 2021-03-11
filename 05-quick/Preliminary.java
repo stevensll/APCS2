e2d2e42974260dcbcb506a51dd2be430bba499a7
Steven Lei <46882137+stevensll@users.noreply.github.com>
2021-03-10 21:48:45 -0500

    tested finding random pivot, swapping it with last element.

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Preliminary{
+    
+      /**Modify the array such that:
+       *1. A random index from start to end inclusive is chosen, the corresponding
+       *   element is designated the pivot element.
+       *2. all elements in range that are smaller than the pivot element are placed before the pivot element.
+       *3. all elements in range that are larger than the pivot element are placed after the pivot element.
+       *4. Only the indices from start to end inclusive are considered in range
+       *@return the index of the final position of the pivot element.
+       **/
+
     public static int partition( int [] data, int start, int end){
-        return 0;
+        Random gen = new Random();
+        int pivot = gen.nextInt(end-start+1) + start;
+        int loSide = start;
+        int hiSide = end-1;
+
+        //first move the pivot to the end
+        int temp = data[end];
+        data[end] = data[pivot];
+        data[pivot] = temp;
+        pivot = end;
+        System.out.println(Arrays.toString(data));
+        for(int i = start; i < end; i++){
+            
+        }
+        return pivot;
     }
 }
--- 05-quick/StevenTester.java
+++ 05-quick/StevenTester.java
@@ -1,5 +1,6 @@
 public class StevenTester {
     public static void main(String[] args) {
-        
+        int [] data = new int[]{999,999,4,3,2,1,9999};
+        System.out.println(Preliminary.partition(data, 2, 5));
     }
 }
