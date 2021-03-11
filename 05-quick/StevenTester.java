6aaf22e8dd8b018984ca34839de92365d5c6c019
Steven Lei <46882137+stevensll@users.noreply.github.com>
2021-03-10 22:38:55 -0500

    tested for expanded array.

--- 05-quick/StevenTester.java
+++ 05-quick/StevenTester.java
@@ -4,7 +4,8 @@ public class StevenTester {
         int [] dataRandom = new int[]{2,3,9,10,0,5,7,2};
         int [] dataBackwards =new int[]{9,8,7,6,5,4,3,2};
         int [] dataSorted =new int[]{1,2,3,4,5,6,7,8};
+        int [] dataBig = new int[]{999,992,991,2,3,9,10,0,5,7,2,992,991,999};
         System.out.println(Arrays.toString(new int[]{0,1,2,3,4,5,6,7}));
-        System.out.println(Preliminary.partition(dataSorted,0, 7));
+        System.out.println(Preliminary.partition(dataBig,3, 10));
     }
 }
