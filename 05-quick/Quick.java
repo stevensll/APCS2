59c4d1f4c4bd40b031e6f4998323515fa66f6264
Steven Lei <46882137+stevensll@users.noreply.github.com>
2021-03-14 22:08:46 -0400

    Created tester method arrGen.

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -3,34 +3,33 @@ public class Quick {
     public static int partition(int[]data ,int lo, int hi){
         Random gen = new Random();
         int pivot = gen.nextInt(hi-lo+1) + lo;
-        // System.out.println(pivot);
         //first move the pivot to the hi
         int temp = data[hi];
         data[hi] = data[pivot];
         data[pivot] = temp;
         pivot = hi;
-        // System.out.println(Arrays.toString(data));
         //use j to track when to increment as we loop through i
         int j = lo -1;
+        boolean left = true;
         for(int i = lo; i < hi; i++){
-            /*if our current value is less than our pivot, we increment j and swap data[j] and data[i]
-            this makes sure that we are always moving elements towards the right if they're less than our pivot
-            */
+            //new condition: if values are the same, 50/50 chance of swapping them.
             if(data[i] <= data[pivot]) {
-                j++;
-                int swapTemp = data[j];
-                data[j] = data[i];
-                data[i] = swapTemp;
+                if(data[i] == data[pivot]&&left){
+                    left = false;
+                } else {
+                    left = true;
+                    j++;
+                    int swapTemp = data[j];
+                    data[j] = data[i];
+                    data[i] = swapTemp;    
+                }
             }
-            // System.out.println(j);
-
         }
         // at the hi of our loop, we swap pivot and data[j+1]
         int lastSwap = data[j+1];
         data[j+1] = data[pivot];
         data[pivot] = lastSwap;
         pivot = j+1;
-        // System.out.println(Arrays.toString(data));
         return pivot;
     }
     
@@ -40,6 +39,9 @@ public class Quick {
     *@postcondition The array can be modified. (If we did not want to modify the array, we could make a copy before we lo the process)
     */
     public static int quickselect(int []data, int k){ 
+        
+        
+        
         return k;
     }
 }
--- 05-quick/StevenTester.java
+++ 05-quick/StevenTester.java
@@ -1,11 +1,60 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 public class StevenTester {
     public static void main(String[] args) {
-        int [] dataRandom = new int[]{2,3,9,10,0,5,7,2};
-        int [] dataBackwards =new int[]{9,8,7,6,5,4,3,2};
-        int [] dataSorted =new int[]{1,2,3,4,5,6,7,8};
-        int [] dataBig = new int[]{999,992,991,2,3,9,10,0,5,7,2,992,991,999};
-        System.out.println(Arrays.toString(new int[]{0,1,2,3,4,5,6,7}));
-        System.out.println(Preliminary.partition(dataBig,3, 10));
+        // int [] dataRandom = new int[]{2,3,3,11,11,5,7,2};// size 8
+        // int [] dataBackwards =new int[]{9,8,7,6,5,4,3,2};
+        // int [] dataSorted =new int[]{1,2,3,4,5,6,7,8};
+        // int [] dataBig = new int[]{999,992,991,2,3,9,10,0,5,7,2,992,991,999};
+        // // System.out.println(Arrays.toString(new int[]{0,1,2,3,4,5,6,7}));
+        // // System.out.println(Quick.partition(dataRandom,0, 7));
+        // // System.out.println(Arrays.toString(dataRandom));
+        // // System.out.println(partitionCheck(dataBackwards));
+        // // System.out.println(partitionCheck(dataRandom));
+        // // System.out.println(partitionCheck(dataSorted));
+        // // System.out.println(partitionCheck(dataBig));
+        int[] test = arrGen(100, "random", 100);
+        System.out.println(Arrays.toString(test));
+    }
+    public static int[] arrGen(int size, String type, int bound){
+        Random gen = new Random();
+        bound = 10000; //by deafult element bound is 10e4
+        int [] arr  = new int[size];
+        for(int i = 0; i < size; i++){
+            if(type.equals("binary")) arr[i] = gen.nextInt(2);
+            else arr[i] = gen.nextInt(bound+1);
+        }
+        if(type.equals("reversed")){
+            int[] arrDesc = Arrays.stream(arr).boxed()
+                            .sorted(Collections.reverseOrder())
+                            .mapToInt(Integer::intValue)
+                            .toArray();
+            arr = arrDesc;
+        }
+        if(type.equals("sorted")){
+            Arrays.sort(arr);
+        }
+        return arr;
+    }
+
+
+    public static boolean partitionCheck(int arr[]){
+        int pivot = Quick.partition(arr, 0, arr.length-1);
+        for(int i = 0; i < arr.length; i++){
+            if(i < pivot){
+                if(!(arr[i] <= arr[pivot])){
+                    System.out.println("failure" + "\n" + Arrays.toString(arr));
+                    return false;
+                }
+            }
+            if(i >= pivot){
+                if(!(arr[i]>=arr[pivot])){
+                    System.out.println("failure" + "\n" + Arrays.toString(arr));
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
