d0c7c06d6e3b074ebfed54cf3a40c4fe47fe2bd7
Steven Lei <46882137+stevensll@users.noreply.github.com>
2021-03-22 18:22:11 -0400

    wrote private resize() and addLast, testing cases for adding to last.

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+import java.util.Arrays;
 public class MyDeque<E>{
     private E[] data;
     private int size, start, end;
@@ -7,7 +9,7 @@ public class MyDeque<E>{
         E[] d = (E[])new Object[10];
         data = d;
         start = 0;
-        end = 0;
+        end = -1;
         size = 0;
     }
 
@@ -16,7 +18,7 @@ public class MyDeque<E>{
         E[] d = (E[])new Object[initialCapacity];
         data = d;
         start = 0;
-        end = 0;
+        end = -1;
         size = 0;
     }
 
@@ -24,23 +26,65 @@ public class MyDeque<E>{
         return size;
     }
     public String toString(){ 
-        String result = "";
+        String result = Arrays.toString(data);
         return result;
     }
-    public void addFirst(E element){ }
-    public void addLast(E element){ }
+    /*
+    Remove/Get (both first and last) will throw: NoSuchElementException - when this deque is empty
+
+Add (both first and last) will throw: NullPointerException - if the specified element is null (this deque does not permit null elements)
+    */
+    public void addFirst(E element) throws NullPointerException{
+        if(element == null) throw new NullPointerException("cannot add a null element");
+        
+    }
+  
+    public void addLast(E element) throws NullPointerException{ 
+        if(element == null) throw new NullPointerException("cannot add a null element");
+        if(size == data.length){
+            //resize and add to end
+            data = resizedArray(this, this.size() * 2);
+            size++;
+            end++;
+            data[end] = element;
+        } else if(end == 921309){
+            //wrap around
+        } else {
+            size++;
+            end++;
+            data[end] = element;
+        }
+    }
+
     public E removeFirst(){ 
         return null;
     }
     public E removeLast(){ 
         return null;
     }
-    
-    public E getFirst(){ 
+    //Remove/Get (both first and last) will throw: NoSuchElementException - when this deque is empty
+
+    public E getFirst () throws NoSuchElementException{ 
+        if(size == 0) throw new NoSuchElementException("deque is empty with size 0");
         return data[start];
     }
-    public E getLast(){ 
+    public E getLast() throws NoSuchElementException{ 
+        if(size == 0) throw new NoSuchElementException("deque is empty with size 0");
         return data[end];
+    }
+
+    //resizes the deque array with a new capacity and flushes all elements to the left
+    private E[] resizedArray (MyDeque<E> deque, int newCapacity){
+        @SuppressWarnings("unchecked")
+        E[] resized = (E[])new Object[newCapacity];
+        for(int i = 0; i < this.size(); i++){
+            if(start+i >= deque.data.length){
+                resized[i] = deque.data[i-size];
+            } else resized[i] = deque.data[start+i];
+        }
+        start = 0;
+        end = this.size()-1;
+        return resized;
     }
 
 }
--- 07-mydeque/StevenDequeTester.java
+++ 07-mydeque/StevenDequeTester.java
@@ -2,5 +2,11 @@ public class StevenDequeTester {
     public static void main(String[] args) {
         MyDeque<Integer> zahando = new MyDeque<Integer>();
         System.out.print(zahando.size());
+        for(int i = 0; i < 15; i++){
+            zahando.addLast(i);
+            System.out.println(zahando);
+            System.out.println(zahando.getFirst());
+            System.out.println(zahando.getLast());
+        }
     }
 }
