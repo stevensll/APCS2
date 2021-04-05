cc374f2b62a813fa4eb713d7d88a9c4757b27687
Steven Lei <46882137+stevensll@users.noreply.github.com>
2021-04-05 00:10:37 -0400

    added too few operands case to tester.

--- 08-calculator/StevenCalculatorTester.java
+++ 08-calculator/StevenCalculatorTester.java
@@ -1,9 +1,10 @@
 public class StevenCalculatorTester {
     public static void main(String[] args) {
         System.out.println(Calculator.eval("11 3 - 4 + 2.5 *"));
-        System.out.println(Calculator.eval("10 2.0 +"));
+        System.out.println(Calculator.eval("10 +"));
         System.out.println(Calculator.eval("8 2 + 99 9 - * 2 + 9 -"));
-        System.out.println(Calculator.eval("1 2 3 4 5 + * - -"));
-
+        System.out.println(Calculator.eval("1 2 3 4 5 % * - -"));
+        System.out.println(Calculator.eval("99 -1 - 10 100 * +"));
+        // System.out.println( 1 - (2 - (3 * (4 % 5))));
     }
 }
