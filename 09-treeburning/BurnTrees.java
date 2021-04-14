7038728f77d7708b2dce99f6906422c087309f5b
Steven Lei <46882137+stevensll@users.noreply.github.com>
2021-04-13 20:06:08 -0400

    updated main. tests look good.

--- 09-treeburning/BurnTrees.java
+++ 09-treeburning/BurnTrees.java
@@ -157,12 +157,7 @@ public class BurnTrees{
     }
     return getTicks();
   }
-  private int solve(){
-      while(!done()){
-          tick();
-      }
-      return getTicks();
-  }
+
 
 
   public static void main(String[]args)  throws InterruptedException{
@@ -179,9 +174,8 @@ public class BurnTrees{
       DELAY = Integer.parseInt(args[3]);
     }
     BurnTrees b = new BurnTrees(WIDTH,HEIGHT,DENSITY);
-
-    // System.out.println(b.solve());
-    System.out.println(b.animate(DELAY));//animate all screens and print the final answer
+    System.out.println(b.run());
+    // System.out.println(b.animate(DELAY));//animate all screens and print the final answer
     // System.out.println(b.outputAll());//print all screens and the final answer
   }
 
